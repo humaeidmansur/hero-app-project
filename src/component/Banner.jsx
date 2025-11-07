5fbfa758504ab04fc565f81d8553a9d55d330dd5
Humaeid Bin Mansur <humaeidmansur@gmail.com>
2025-11-07 23:59:46 +0100

    Added new component Banner and Rating and something

---
+++ src/component/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import banner from "../assets/hero.png"
+import play_store from "../assets/play_store.png"
+import app_store from "../assets/App.png"
+
+const Banner = () => {
+    return (
+        <div  className='flex justify-center items-center mt-5'>
+            <div>
+                <h1 className='flex justify-center items-center font-bold text-[40px]'>We Build</h1> 
+                 <h1 className='flex justify-center items-center font-bold text-[40px]'> 
+                    <span className='mr-3 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>Productive</span> Apps</h1>
+                <p className='flex justify-center items-center text-center mt-5 mb-5'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.
+                    <br/> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+             
+                <div className=" mr-0 md:mr-10 flex justify-center items-center gap-5">
+                  <a className="btn"><img src={play_store} alt=""/>Contribute</a>
+                  <a className="btn"><img src={app_store} alt=""/>Contribute</a>
+                </div>
+
+                <img className='mt-5' src={banner} alt="" />
+              </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/component/Rating.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Rating = () => {
+    return (
+       <footer className="footer footer-horizontal footer-center  bg-gradient-to-r from-[#632EE3] to-[#9F62F2]  text-primary-content p-10">
+  <aside>
+ 
+    <p className="font-bold text-[20px] md:text-[30px]">
+    Trusted by Millions, Built for You  </p>
+
+     
+
+  </aside>
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+      <a>
+            <p>Total Downloads</p>
+            <h1 className='font-bold text-[40px] mb-5 mt-5'>29.6M</h1>
+            <p>21% more than last month</p>
+ 
+      </a>
+      <a>
+            <p>Total Reviews</p>
+            <h1 className='font-bold text-[40px] mb-5 mt-5'>906K</h1>
+            <p>46% more than last month</p>
+      </a>
+      <a>
+            <p>Active Apps</p>
+            <h1 className='font-bold text-[40px] mb-5 mt-5'>132+</h1>
+            <p>31 more will Launch</p>
+      </a>
+    </div>
+  </nav>
+</footer>
+    );
+};
+
+export default Rating;
